cbf91b4fa3f77ec430bf1a6957e739fff86d4e0d
colinporth <colinporth@users.noreply.github.com>
2019-12-19 22:15:07 +0000

    start off winTv - windows openGLK tv grab project

---
+++ winTv/main.cpp
@@ -0,0 +1,138 @@
+// winTv.cpp
+//{{{  includes
+#define _CRT_SECURE_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#include <windows.h>
+
+#include <winsock2.h>
+#include <WS2tcpip.h>
+
+#include <thread>
+#include <chrono>
+
+#include "../../shared/utils/date.h"
+#include "../../shared/utils/utils.h"
+#include "../../shared/utils/cLog.h"
+#include "../../shared/utils/cSemaphore.h"
+
+#include "../../shared/utils/iChange.h"
+
+#include "../../shared/dvb/cWinDvb.h"
+
+#include "../../shared/nanoVg/cGlWindow.h"
+#include "../../shared/fonts/FreeSansBold.h"
+
+#include "../../shared/widgets/cTextBox.h"
+#include "../../shared/widgets/cValueBox.h"
+#include "../../shared/widgets/cSelectText.h"
+#include "../../shared/widgets/cNumBox.h"
+#include "../../shared/widgets/cTransportStreamBox.h"
+
+using namespace std;
+//}}}
+
+class cAppWindow : public cGlWindow {
+public:
+  cAppWindow() {}
+  //{{{
+  void run (string title, int width, int height, int frequency) {
+
+    string rootName = "/tv";
+    mFrequency  = frequency;
+    cLog::log (LOGINFO, "run %d", mFrequency);
+
+    mDvb = new cDvb (rootName);
+    mDvb->createGraph (frequency);
+
+    auto root = cGlWindow::initialise (title, width, height, (unsigned char*)freeSansBold);
+    add (new cTransportStreamBox (mDvb, 0.f, 0.f));
+
+    thread ([=]() { mDvb->grabThread(); } ).detach();
+
+    glClearColor (0, 0, 0, 1.f);
+    cGlWindow::run();
+
+    delete mDvb;
+
+    cLog::log (LOGINFO, "run exit");
+    }
+  //}}}
+
+protected:
+  //{{{
+  void onKey (int key, int scancode, int action, int mods) {
+
+    //mods == GLFW_MOD_SHIFT
+    //mods == GLFW_MOD_CONTROL
+    if ((action == GLFW_PRESS) || (action == GLFW_REPEAT)) {
+      switch (key) {
+        case GLFW_KEY_ESCAPE: glfwSetWindowShouldClose (mWindow, GL_TRUE); break;
+        case GLFW_KEY_SPACE : break;
+        case GLFW_KEY_1:
+        case GLFW_KEY_2:
+        case GLFW_KEY_3:
+        case GLFW_KEY_4:
+        case GLFW_KEY_5:
+        case GLFW_KEY_6: break;
+
+        case GLFW_KEY_RIGHT: break;
+        case GLFW_KEY_LEFT:  break;
+        case GLFW_KEY_DOWN:  break;
+        case GLFW_KEY_UP:    break;
+        case GLFW_KEY_PAGE_UP:   break;
+        case GLFW_KEY_PAGE_DOWN: break;
+        //case GLFW_KEY_HOME:
+        //case GLFW_KEY_END:
+
+        case GLFW_KEY_V: toggleVsync(); break;
+        case GLFW_KEY_P: togglePerf(); break;
+        case GLFW_KEY_S: toggleStats(); break;
+        case GLFW_KEY_T: toggleTests(); break;
+
+        case GLFW_KEY_I: toggleSolid(); break;
+        case GLFW_KEY_A: toggleEdges(); break;
+        case GLFW_KEY_Q: fringeWidth (getFringeWidth() - 0.25f); break;
+        case GLFW_KEY_W: fringeWidth (getFringeWidth() + 0.25f); break;
+
+        case GLFW_KEY_L:
+          mLogInfo = ! mLogInfo;
+          cLog::setLogLevel (mLogInfo ? LOGINFO3 : LOGNOTICE);
+          break;
+
+        default: cLog::log (LOGNOTICE, "Keyboard %x", key); break;
+        }
+      }
+    }
+  //}}}
+  void onChar (char ch, int mods) {}
+
+private:
+  bool mLogInfo = true;
+  int mFrequency = 0;
+  cDvb* mDvb = nullptr;
+  };
+
+
+int main (int argc, char* argv[]) {
+
+  CoInitializeEx (NULL, COINIT_MULTITHREADED);
+
+  bool logInfo = false;
+  int frequency  = 626;
+
+  for (auto arg = 1; arg < argc; arg++)
+    if (!strcmp(argv[arg], "l")) logInfo = true;
+    else if (!strcmp(argv[arg], "f")) frequency = 674;
+
+  cLog::init (logInfo ? LOGINFO3 : LOGINFO, false, "");
+  cLog::log (LOGNOTICE, "winTv log:" + dec(logInfo) +
+                        " frequency:" + dec(frequency));
+
+  cAppWindow appWindow;
+  appWindow.run ("tv", 800, 480, frequency * 1000);
+
+  CoUninitialize();
+  return 0;
+  }
