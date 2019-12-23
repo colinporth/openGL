f6f51932f4cc21a9f7973fb761bb143b18774ed1
colinporth <colinporth@users.noreply.github.com>
2019-12-23 12:20:08 +0000

    fix

--- tv/tvGrab.cpp
+++ tv/tvGrab.cpp
@@ -6,8 +6,7 @@
 #include <math.h>
 
 #include <thread>
-#include <chrono>
-//#include <string>
+#include <string>
 #include "../../shared/utils/date.h"
 
 #include "../../shared/nanoVg/cGlWindow.h"
@@ -32,18 +31,18 @@ class cAppWindow : public cGlWindow {
 public:
   cAppWindow() {}
   //{{{
-  void run (const string& title, int width, int height, unsigned int frequency, const string& root) {
+  void run (const string& title, int width, int height, unsigned int frequency) {
+
+    mDvb = new cDvb (frequency*1000, "home/pi/ts", true);
 
     initialise (title, width, height, (unsigned char*)droidSansMono);
-    add (new cTextBox (mPacketStr, 15.f));
-    add (new cTextBox (mDvb.mSignalStr, 14.f));
-    add (new cTextBox (mDvb.mTuneStr, 12.f));
-    addAt (new cTransportStreamBox (&mDvb, 0.f, -2.f), 0.f, 1.f);
-
-    mDvb = new cDvb (frequency, root);
+    add (new cTextBox (mDvb->mErrorStr, 12.f));
+    add (new cTextBox (mDvb->mTuneStr, 12.f));
+    add (new cTextBox (mDvb->mSignalStr, 16.f));
+    addAt (new cTransportStreamBox (mDvb, 0.f, -2.f), 0.f, 1.f);
 
     // launch dvbThread
-    auto dvbCaptureThread = thread ([=]() { mDvb->captureThread (frequency); });
+    auto dvbCaptureThread = thread ([=]() { mDvb->captureThread(); });
     sched_param sch_params;
     sch_params.sched_priority = sched_get_priority_max (SCHED_RR);
     pthread_setschedparam (dvbCaptureThread.native_handle(), SCHED_RR, &sch_params);
@@ -54,6 +53,8 @@ public:
 
     glClearColor (0, 0, 0, 1.f);
     cGlWindow::run();
+
+    delete mDvb;
 
     cLog::log (LOGINFO, "run exit");
     }
@@ -92,43 +93,26 @@ protected:
   void onChar (char ch, int mods) {}
 
 private:
-  //{{{  vars
+  bool mLogInfo = false;
   cDvb* mDvb;
-
-  string mStr1 = "tv app";
-  string mStr2 = "";
-  string mStr3 = "";
-
-  uint64_t mLastDiscontinuity = 0;
-  int mLastBlockSize = 0;
-  int mMaxBlockSize = 0;
-  string mPacketStr = "packet";
-
-  bool mLogInfo = true;
-  //}}}
   };
-
 
 int main (int argc, char* argv[]) {
 
   bool logInfo = false;
   int frequency = 626;
-  string root = "home/pi/ts";
-
   for (auto arg = 1; arg < argc; arg++)
     if (!strcmp(argv[arg], "l")) logInfo = true;
     else if (!strcmp(argv[arg], "itv")) frequency = 650;
     else if (!strcmp(argv[arg], "bbc")) frequency = 674;
     else if (!strcmp(argv[arg], "hd"))  frequency = 626;
-    else if (!strcmp(argv[arg], "r")) root = argv[++arg];
 
   cLog::init (logInfo ? LOGINFO3 : LOGINFO, false, "");
   cLog::log (LOGNOTICE, "linTvGrab logInfo:" + dec(logInfo) +
-                        " freq:" + dec(frequency) +
-                        " root:" + root);
+                        " freq:" + dec(frequency));
 
-  cAppWindow appWindow (root);
-  appWindow.run ("tv - openGL2", 790, 400, frequency, root);
+  cAppWindow appWindow;
+  appWindow.run ("tv - openGL2", 790, 400, frequency);
 
   return 0;
   }
