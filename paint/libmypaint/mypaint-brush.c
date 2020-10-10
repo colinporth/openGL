ca39e4f2b94e57e24ca2f18b6a6f9e52153a9eee
colinporth <colinporth@users.noreply.github.com>
2020-10-10 12:51:23 +0100

    start of paint testbed

--- paint/libmypaint/mypaint-brush.c
+++ paint/libmypaint/mypaint-brush.c
@@ -37,7 +37,7 @@
 #include "helpers.h"
 #include "rng-double.h"
 
-#include <json.h>
+#include <json-c/json.h>
 
 #ifdef _MSC_VER
 #if _MSC_VER < 1700     // Visual Studio 2012 and later has isfinite and roundf
