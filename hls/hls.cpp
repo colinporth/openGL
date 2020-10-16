f4c82be75cdc4ad89b26b496d47657220af36916
colinporth <colinporth@users.noreply.github.com>
2020-10-16 14:41:06 +0100

    fiddle

--- hls/hls.cpp
+++ hls/hls.cpp
@@ -60,7 +60,9 @@ public:
 
     cLoaderPlayer::initialise (radio,
       radio ? kRadioHost : kTvHost, radio ? "pool_904/live/uk/" : "pool_902/live/uk/", channelName,
-      audBitrate, vidBitrate, eLoader (eMfx | eQueueAudio | eQueueVideo));
+      audBitrate, vidBitrate, 
+      eLoader (eQueueAudio | eQueueVideo));
+      //eLoader (eMfx | eQueueAudio | eQueueVideo));
 
     if (headless) {
       thread ([=](){ hlsLoaderThread(); }).detach();
