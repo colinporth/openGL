490424c6801aa152369b370f14abd696b959bdc3
colinporth <colinporth@users.noreply.github.com>
2020-10-16 15:32:35 +0100

    Update hls.cpp

--- hls/hls.cpp
+++ hls/hls.cpp
@@ -60,7 +60,7 @@ public:
 
     cLoaderPlayer::initialise (radio,
       radio ? kRadioHost : kTvHost, radio ? "pool_904/live/uk/" : "pool_902/live/uk/", channelName,
-      audBitrate, vidBitrate, 
+      audBitrate, vidBitrate,
       eLoader (eQueueAudio | eQueueVideo));
       //eLoader (eMfx | eQueueAudio | eQueueVideo));
 
